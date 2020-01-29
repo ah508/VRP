9fde22e91513f56249428c396b3095a1a011f884
Adam Horn <ahorn508@gmail.com>
2020-01-28 20:14:33 -0500

    can now compute and display separated routes - next step is SEARCH implementation

--- example.py
+++ example.py
@@ -5,6 +5,7 @@ import numpy as np
 import time
 from genius_test import GENIUS
 from useful_funcs import PointGrab
+from proc_func import separate
 
 point_set = PointGrab()
 
@@ -13,6 +14,7 @@ n_time = time.process_time()
 momentotruth = GENIUS(point_set)
 momentotruth.cycle()
 momentotruth.post_opt()
+held_history = momentotruth.history
 e_time = time.process_time() - n_time
 print(e_time)
 
@@ -20,20 +22,52 @@ print(e_time)
 def parse_history(history):
     parsed_hist = []
     for moment in history:
-        xy = np.zeros((len(moment)+2, 2))
-        xy[0, 0] = 0
-        xy[0, 1] = 0
-        nextvert = 0
-        for vertex in range(0, len(moment)+1):
-            xy[vertex, 0] = point_set.xpoints[nextvert]
-            xy[vertex, 1] = point_set.ypoints[nextvert]
-            nextvert = moment[nextvert]
-        
+        xy = []
+        # if len(moment) > 1:
+            # print('sep hit')
+        xy_app = []
+        for path in moment:
+            # print(path)
+            if set(path) == set([None]):
+                # print('safety hit')
+                continue
+            # else:
+            #     print('no safety hit')
+            # xy_app = np.zeros((1, 2))
+            xy_app_app = np.array([point_set.xpoints[0], point_set.ypoints[0]])
+            nextvert = path[0]
+            try:
+                xy_app = np.vstack((xy_app, xy_app_app))
+            except ValueError:
+                xy_app = xy_app_app
+            xy_app_app = np.array([point_set.xpoints[nextvert], point_set.ypoints[nextvert]])
+            nextvert = path[nextvert]
+            xy_app = np.vstack((xy_app, xy_app_app))
+            loopsafe = 0
+            while nextvert != path[0] and loopsafe < 1000:
+                # print(nextvert)
+                xy_app_app = np.array([point_set.xpoints[nextvert], point_set.ypoints[nextvert]])
+                nextvert = path[nextvert]
+                xy_app = np.vstack((xy_app, xy_app_app))
+                loopsafe += 1
+            xy_app_app = np.array([point_set.xpoints[0], point_set.ypoints[0]])
+            xy_app = np.vstack((xy_app, xy_app_app))
+            try:
+                xy = np.vstack((xy, xy_app))
+            except ValueError:
+                xy = xy_app
         parsed_hist.append(xy)
     return parsed_hist
 
-print(momentotruth.history[-1])
-history = parse_history(momentotruth.history)
+# for i in range(0, len(history)):
+#     print(momentotruth.history[i])
+#     print(history[i])
+# print(history[-1])
+pathlist, pathdirectory = separate(held_history[-1][0].copy(), point_set.d_matrix, 25, 5)
+held_history.append(pathlist)
+# print(pathlist)
+history = parse_history(held_history)
+# print(history)
 
 fig, ax = plt.subplots()
 ax.set(xlim=(-5.1, 5.1), ylim=(-5.1, 5.1))
@@ -46,5 +80,5 @@ def animate(frame):
     shape.set_xy(history[frame])
     return shape,
 
-animation = animation.FuncAnimation(fig, animate, frames=len(momentotruth.history)-1, interval=300, repeat_delay=5000)
+animation = animation.FuncAnimation(fig, animate, frames=len(held_history), interval=200, repeat_delay=10000)
 plt.show()
--- proc_func.py
+++ proc_func.py
@@ -1,78 +1,288 @@
 import numpy as np
+import math
 
-def find_successor(vertex, edges):
-    if isinstance(vertex, int) or isinstance(vertex, np.int64):
-        return edges[vertex]
-    elif isinstance(vertex, list):
-        successors = {}
-        for point in vertex:
-            successors[point] = edges[point]
-        return successors
-    else:
-        print('oops, something went wrong with finding successors.')
-        print(type(vertex))
-        input(':')
-        return None
+class GenFunc:
+    """A set of general functions intended for subclassing.
 
-def find_predecessor(vertex, edges):
-    if isinstance(vertex, int) or isinstance(vertex, np.int64):
-        return edges.index(vertex)
-    elif isinstance(vertex, list):
-        predecessors = {}
-        for point in vertex:
-            predecessors[point] = edges.index(point)
-        return predecessors
-    else:
-        print('oops, something went wrong with finding predecessors.')
-        print(type(vertex))
-        input(':')
-        return None
+    WARNING: ENSURE THAT self.dist IS DEFINED IN THE CHILD CLASS
+    """
+    
+    def __init__(self):
+        self.checkinit()
 
-def reverse(edges, start, end):
-    holding = edges.copy()
-    reverse_recurse(holding, start, end)
-    return holding
-    #might be improved by just returning a diff
-    #would remove this as a middleman
+    def checkinit(self):
+        if hasattr(self, 'dist'):
+            pass
+        else:
+            raise NotImplementedError('distance is not defined')
 
-def reverse_recurse(edges, start, end):
-    tail = find_successor(start, edges)
-    if tail == end:
-        edges[end] = start
-        return start
-    else:
-        new_end = reverse_recurse(edges, tail, end)
-        edges[new_end] = start
-        return start
-
-def points_between(edges, v1, v2):
-    if v1 == v2:
-        return []
-    return [v1] + points_between(edges, find_successor(v1, edges), v2)
-
-def test_valid(edgeset, indicator='+'):
-    tester = set()
-    for i in edgeset:
-        if i in tester and i != None:
-            print('invalid construction!')
-            print(f'flag: {indicator}')
-            print(edgeset)
+    def find_successor(self, vertex, edges):
+        if isinstance(vertex, int) or isinstance(vertex, np.int64):
+            return edges[vertex]
+        elif isinstance(vertex, list):
+            successors = {}
+            for point in vertex:
+                successors[point] = edges[point]
+            return successors
+        else:
+            print('oops, something went wrong with finding successors.')
+            print(type(vertex))
             input(':')
-            return False
-        if i == edgeset.index(i):
-            print('formed a loop!')
-            print(f'flag: {indicator}')
-            print(edgeset)
+            return None
+
+    def find_predecessor(self, vertex, edges):
+        if isinstance(vertex, int) or isinstance(vertex, np.int64):
+            return edges.index(vertex)
+        elif isinstance(vertex, list):
+            predecessors = {}
+            for point in vertex:
+                predecessors[point] = edges.index(point)
+            return predecessors
+        else:
+            print('oops, something went wrong with finding predecessors.')
+            print(type(vertex))
             input(':')
-            return False
-        try:
-            if edgeset[i] == None:
-                print('invalid pointer')
+            return None
+
+    def reverse(self, edges, start, end):
+        holding = edges.copy()
+        self.reverse_recurse(holding, start, end)
+        return holding
+        #might be improved by just returning a diff
+        #would remove this as a middleman
+
+    def reverse_recurse(self, edges, start, end):
+        tail = self.find_successor(start, edges)
+        if tail == end:
+            edges[end] = start
+            return start
+        else:
+            new_end = self.reverse_recurse(edges, tail, end)
+            edges[new_end] = start
+            return start
+
+    def points_between(self, edges, v1, v2):
+        if v1 == v2:
+            return []
+        return [v1] + self.points_between(edges, self.find_successor(v1, edges), v2)
+
+    def test_valid(self, edgeset, indicator='+'):
+        tester = set()
+        for i in edgeset:
+            if i in tester and i != None:
+                print('invalid construction!')
                 print(f'flag: {indicator}')
                 print(edgeset)
                 input(':')
                 return False
-        except TypeError:
-            pass
-        tester.add(i)
-    return True
+            if i == edgeset.index(i):
+                print('formed a loop!')
+                print(f'flag: {indicator}')
+                print(edgeset)
+                input(':')
+                return False
+            try:
+                if edgeset[i] == None:
+                    print('invalid pointer')
+                    print(f'flag: {indicator}')
+                    print(edgeset)
+                    input(':')
+                    return False
+            except TypeError:
+                pass
+            tester.add(i)
+        return True
+
+    def best_candidate(self, candidates):
+        min_pair = ['x', math.inf]
+        for key in candidates.keys():
+            if candidates[key] != None:
+                if candidates[key]['cost'] <= min_pair[1]:
+                    min_pair = [key, candidates[key]['cost']]
+        return min_pair
+
+    def circuit_cost(self, circuit):
+        cost = 0
+        for vertex, pointer in enumerate(circuit):
+            if pointer != None:
+                cost += self.dist[vertex, pointer]
+        return cost
+
+    def t1_string(self, pointers, i, j, k, v):
+        move = {}
+        successor = self.find_successor([i, j, k], pointers)
+        move['frame'] = pointers.copy()
+        if successor[i] != j:
+            move['frame'] = self.reverse(move['frame'], successor[i], j)
+        if successor[j] != k:
+            move['frame'] = self.reverse(move['frame'], successor[j], k)
+        move['frame'][i] = v
+        move['frame'][v] = j
+        move['frame'][successor[i]] = k
+        move['frame'][successor[j]] = successor[k]
+        test = self.test_valid(move['frame'])
+        if not test:
+            return None
+        move['cost'] = self.circuit_cost(move['frame'])
+        return move
+
+    def t2_string(self, pointers, i, j, k, l, v):
+        move = {}
+        successor = self.find_successor([i, j], pointers)
+        predecessor = self.find_predecessor([k, l], pointers)
+        move['frame'] = pointers.copy()
+        if successor[i] != predecessor[l]:
+            move['frame'] = self.reverse(move['frame'], successor[i], predecessor[l])
+        if l != j:
+            move['frame'] = self.reverse(move['frame'], l, j)
+        move['frame'][i] = v
+        move['frame'][v] = j
+        move['frame'][l] = successor[j]
+        move['frame'][predecessor[k]] = predecessor[l]
+        move['frame'][successor[i]] = k
+        test = self.test_valid(move['frame'])
+        if not test:
+            return None
+        move['cost'] = self.circuit_cost(move['frame'])
+        return move
+
+# def insert_vertex(self, vertex, edgeset, direction=False):
+#     possible_moves = {}
+#     move_key = 0
+#     vi = self.p_neighborhood[vertex]
+#     vj = self.p_neighborhood[vertex]
+#     if direction:
+#         direct = [edgeset]
+#     else:
+#         direct = [edgeset, self.reverse(edgeset, 0, 0)]
+#     for d_set in direct:
+#         for i in vi:
+#             if i == vertex:
+#                 continue
+#             vi1 = self.find_successor(i, d_set)
+#             for j in vj:
+#                 if j == vertex:
+#                     continue
+#                 if i == j:
+#                     continue
+#                 j_to_i = points_between(d_set, j, i)
+#                 i_to_j = points_between(d_set, i, j)
+#                 vj1 = self.find_successor(j, d_set)
+#                 vk = self.p_neighborhood[vi1]
+#                 vl = self.p_neighborhood[vj1]
+#                 for k in vk:
+#                     if k == vertex:
+#                         continue
+#                     if j == k:
+#                         continue
+#                     if k not in j_to_i:
+#                         continue
+#                     possible_moves[move_key] = self.t1_string(d_set, i, j, k, vertex)
+#                     move_key += 1
+#                     for l in vl:
+#                         if l == vertex:
+#                             continue
+#                         if i == l:
+#                             continue
+#                         if l not in i_to_j:
+#                             continue
+#                         if k != vj1 and l != vi1:
+#                             possible_moves[move_key] = self.t2_string(d_set, i, j, k, l, vertex)
+#                             move_key += 1
+#     min_pair = ['x', math.inf]
+#     for key in possible_moves.keys():
+#         if possible_moves[key] != None:
+#             if possible_moves[key]['cost'] <= min_pair[1]:
+#                 min_pair = [key, possible_moves[key]['cost']]
+#     if direction:
+#         return possible_moves[min_pair[0]]
+#     else:
+#         self.swap(vertex)
+#         self.execute_move(possible_moves[min_pair[0]])
+#         possible_moves = {}
+#         return None
+
+    def t1_unstring(self, pointers, j, k, v):
+        successor = self.find_successor([v, j, k], pointers)
+        predecessor = self.find_predecessor([v, j, k], pointers)
+        move = pointers.copy()
+        if successor[v] != k:
+            move = self.reverse(move, successor[v], k)
+        if successor[k] != j:
+            move = self.reverse(move, successor[k], j)
+        move[predecessor[v]] = k
+        move[successor[v]] = j
+        move[successor[k]] = successor[j]
+        move[v] = None
+        test = self.test_valid(move, indicator='+')
+        if not test:
+            return None
+        return move
+
+    def t2_unstring(self, pointers, j, k, l, v):
+        successor = self.find_successor([v, j, k, l], pointers)
+        predecessor = self.find_predecessor([v, j, k, l], pointers)
+        move = pointers.copy()
+        if successor[v] != predecessor[j]:
+            move = self.reverse(move, successor[v], predecessor[j])
+        if successor[l] != k:
+            move = self.reverse(move, successor[l], k)
+        move[predecessor[v]] = k
+        move[successor[l]] = predecessor[j]
+        move[successor[v]] = j
+        move[l] = successor[k]
+        move[v] = None
+        test = self.test_valid(move, indicator='-')
+        if not test:
+            return None
+        return move
+
+def separate(path, distances, constraint, path_num):
+    # print('something is probably wrong with distance checking')
+    path_directory = [None] * len(path)
+    path_list = [None] * path_num
+    vertices_remaining = len(path) - 1
+    for loop in range(0, path_num):
+        arbpath = [None] * len(path)
+        valid_const = True
+        index_track = 0
+        dist_track = 0
+        home_val = 0
+        safety = 0
+        while vertices_remaining > 0 and valid_const and safety < 1000:
+            d_check = dist_track + distances[index_track, path[index_track]]
+            l_check = d_check + distances[path[index_track], 0]
+            if l_check <= constraint:
+                dist_track += distances[index_track, path[index_track]]
+                arbpath[index_track] = path[index_track]
+                path[index_track] = None
+                if index_track != 0:
+                    path_directory[index_track] = loop
+                    vertices_remaining -= 1
+                index_track = arbpath[index_track]
+            elif loop != path_num-1:
+                dist_track += home_val
+                arbpath[index_track] = 0
+                path[0] = path[index_track]
+                valid_const = False
+                if index_track != 0:
+                    path_directory[index_track] = loop
+                    vertices_remaining -= 1
+            else:
+                dist_track += d_check
+                arbpath[index_track] = path[index_track]
+                path[index_track] = None
+                if index_track != 0:
+                    path_directory[index_track] = loop
+                    vertices_remaining -= 1
+                index_track = arbpath[index_track]
+            home_val = distances[arbpath[index_track], 0]
+            safety += 1
+        if safety == 1000:
+            raise ResourceWarning('potentially infinite loop')
+        path_list[loop] = arbpath
+    return path_list, path_directory
+
+# def get_dist(dist, v1, v2):
+#     return dist[v1, v2]
+
